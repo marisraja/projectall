920293a0cbc4c1de0a42035a015f3b58e5f338c5
marisraja <marisraja>
2022-07-15 16:02:20 +0530

    projectnew

---
+++ src/org/ctrl/Hello.java
@@ -0,0 +1,18 @@
+package org.ctrl;
+
+public class Hello {
+public static void main(String[] args) {
+	int u=20, v=13,w=14;
+	if (u>v&&u>v) {
+		System.out.println("u is Red");
+		}
+	else if (v>u&&u>w) {
+		System.out.println("v is  Blue");
+	}
+	else if (w>u&&w>v) {
+		System.out.println("w is  green");
+	}
+}
+}
+
+
---
+++ src/org/ctrl/Switch.java
@@ -0,0 +1,23 @@
+package org.ctrl;
+
+public class Switch {
+	public static void main(String[] args) {
+		int height=1;
+		switch (height) {
+		case 1:
+			System.out.println("person:1"+100);
+			
+			break;
+		case 2:
+			System.out.println("person:2."+200);
+			
+			break;
+
+		default:
+			System.out.println("not");
+			break;
+		}
+		
+	}
+}
+  
